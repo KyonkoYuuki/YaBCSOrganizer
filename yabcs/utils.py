db37d009fbaab9a70683a12c67dce37f6beb3c34
KyonkoYuuki <kyonkoyuuki@users.noreply.github.com>
2020-05-25 20:21:01 -0400

    Added missing emb_name to find/replace

--- yabcs/utils.py
+++ yabcs/utils.py
@@ -3,8 +3,8 @@ from pyxenoverse.bcs.color_selector import ColorSelector
 from pyxenoverse.bcs.physics import Physics
 
 FIND_ITEM_TYPES = [
-    (Part, ['name', "model", "model2", "texture", "emd_name", "emm_name", "ean_name", "dyt_options", "part_hiding"]),
-    (Physics, ['name', "texture", "emd_name", "emm_name", "esk_name", "bone_name", "scd_name", "dyt_options", "part_hiding"]),
+    (Part, ['name', "model", "model2", "texture", "emd_name", "emm_name", "emb_name", "ean_name", "dyt_options", "part_hiding"]),
+    (Physics, ['name', "texture", "emd_name", "emm_name", "emb_name", "esk_name", "bone_name", "scd_name", "dyt_options", "part_hiding"]),
     (ColorSelector, ['part_colors', 'color']),
 ]
 
